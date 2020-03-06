479e394cd943c11fe7fc8b812d35e1b40de75c42
Aastha-byte <59638445+Aastha-byte@users.noreply.github.com>
2020-03-06 12:17:40 -0700

    edit

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 const moment = require('moment');
+const gallery = require('./gallery');
 
 const app = express();
 
 app.use(function(req, res, next) {
     res.locals.year = moment().format('YYYY');
+    res.locals.gallery=gallery;
     next();
   });
 
@@ -22,10 +24,18 @@ app.get('/about', function(req,res){
     res.render('about',{title: 'About'});
 });
 
-app.get('/gallery', function(req,res){
-    res.render('gallery',{title: 'Gallery'});
+app.get('/gallery', function (req, res) {
+    res.render("gallery", { title: 'Gallery'
+    });
 });
-
+app.get('/gallery/:id', function (req, res,next) {
+    for (x of gallery) {
+        if(`${req.params.id}` == x.id) {
+            res.render("gallerybyid", {title: `${req.params.id}`})
+        return;
+    }}
+    next();
+});
 app.get('/contact', function(req,res){
     res.render('contact',{title: 'Contact'});
 });
